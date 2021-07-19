1a42f1f33ce69ba3037238339f27f445fbaa4c8d
Januar Ikmal <ikmal.dev@gmail.com>
2021-07-19 10:48:39 +0700

    implementing favorite condition

--- app/src/main/java/com/ikmal/androidsehatqtest/features/home/view/viewholder/ProductViewHolder.kt
+++ app/src/main/java/com/ikmal/androidsehatqtest/features/home/view/viewholder/ProductViewHolder.kt
@@ -1,6 +1,8 @@
 package com.ikmal.androidsehatqtest.features.home.view.viewholder
 
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.ikmal.androidsehatqtest.R
 import com.ikmal.androidsehatqtest.core.extension.loadImage
 import com.ikmal.androidsehatqtest.databinding.ItemProductBinding
 import com.ikmal.androidsehatqtest.data.api.model.ProductPromo
@@ -15,6 +17,21 @@ class ProductViewHolder(
             productImageList.loadImage(product.imageUrl)
             productName.text = product.title
             productContentList.text = product.description
+            if (product.loved > 0) {
+                productBtnFavList.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        root.context,
+                        R.drawable.ic_favorite_red
+                    )
+                )
+            } else {
+                productBtnFavList.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        root.context,
+                        R.drawable.ic_favorite
+                    )
+                )
+            }
             productRootCard.setOnClickListener {
                 onItemClick.invoke(product)
             }
