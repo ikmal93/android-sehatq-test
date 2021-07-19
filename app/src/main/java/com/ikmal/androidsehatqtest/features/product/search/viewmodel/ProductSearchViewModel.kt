9d88e910001cc0d13ac8d7819bb904fc4e0ce644
Januar Ikmal <ikmal.dev@gmail.com>
2021-07-19 13:30:03 +0700

    remove unused import

--- app/src/main/java/com/ikmal/androidsehatqtest/features/product/search/viewmodel/ProductSearchViewModel.kt
+++ app/src/main/java/com/ikmal/androidsehatqtest/features/product/search/viewmodel/ProductSearchViewModel.kt
@@ -1,11 +1,10 @@
 package com.ikmal.androidsehatqtest.features.product.search.viewmodel
 
-import android.util.Log
-import androidx.lifecycle.*
-import com.google.gson.Gson
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.ikmal.androidsehatqtest.core.utils.Resource
-import com.ikmal.androidsehatqtest.data.api.model.ProductPromo
-import com.ikmal.androidsehatqtest.data.api.repository.DataRepository
 import com.ikmal.androidsehatqtest.data.local.model.entity.productpromo.ProductPromoEntity
 import com.ikmal.androidsehatqtest.data.local.repository.product.ProductPromoRepository
 import kotlinx.coroutines.launch
@@ -47,7 +46,6 @@ class ProductSearchViewModel(
 
     private fun refreshProduct() {
         val filterKeywordValue = filterKeyword.value ?: ""
-
         val filteredPromo = promoProducts.value?.data?.let {
             it.filter { data ->
                 data.title.lowercase().contains(filterKeywordValue.lowercase())
